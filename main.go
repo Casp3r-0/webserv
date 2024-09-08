d1f455c17a3ece9d9d4295910d4fc77576ece638
Casp3r-0 <michzadc@gmail.com>
2024-09-08 15:20:14 -0500

    moved healthz and metrics to own API namespace

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ func main() {
 	}
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	//	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("GET /healthz/", handler)
-	mux.HandleFunc("GET /metrics/", apiCfg.countHandler)
+	mux.HandleFunc("GET /api/healthz/", handler)
+	mux.HandleFunc("GET /api/metrics/", apiCfg.countHandler)
 	mux.HandleFunc("/reset/", apiCfg.resetHandler)
 
 	log.Printf("Serving on port: %s\n", port)
