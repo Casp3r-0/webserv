1c7dbb9683d5d254c03b2e2b763537804ef50b26
Casp3r-0 <michzadc@gmail.com>
2024-09-08 14:45:18 -0500

    added metrics handler, reset handler

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+
+}
+
+func (cfg *apiConfig) countHandler(w http.ResponseWriter, r *http.Request) {
+
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	w.Write([]byte(hits))
+
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits++
+	return next
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+}
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	var apiCfg apiConfig
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("."))))
+	//	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz/", handler)
+	mux.HandleFunc("/metrics/", apiCfg.countHandler)
+	mux.HandleFunc("/reset/", apiCfg.resetHandler)
+
+	log.Printf("Serving on port: %s\n", port)
+	srv.ListenAndServe()
+
+}
