d1003b2f35b65a609832d40e47da2291cefc6cd8
Casp3r-0 <michzadc@gmail.com>
2024-09-10 09:01:09 -0500

    reorged functions. created metrics.go, readiness.go, reset.go

--- main.go
+++ main.go
@@ -2,42 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte("OK"))
-
-}
-
-func (cfg *apiConfig) countHandler(w http.ResponseWriter, r *http.Request) {
-
-	html := fmt.Sprintf("<html>\n\n<body>\n <h1>Welcome, Chirpy Admin</h1>\n  <p>Chirpy has been visited %d times!</p>\n</body>\n\n</html>", cfg.fileserverHits)
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(html))
-
-}
-
-type apiConfig struct {
-	fileserverHits int
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits = 0
-}
 
 func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
---
+++ metrics.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (cfg *apiConfig) countHandler(w http.ResponseWriter, r *http.Request) {
+
+	html := fmt.Sprintf("<html>\n\n<body>\n <h1>Welcome, Chirpy Admin</h1>\n  <p>Chirpy has been visited %d times!</p>\n</body>\n\n</html>", cfg.fileserverHits)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(html))
+
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
